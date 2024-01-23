97d0c4ec699f5a76690e41e318b04fc4ee34c3fa
Clémence Robert <clemence.robert@complifegroup.com>
2024-01-23 15:01:42 +0100

    this is my first R script

---
+++ my_first_code.R
@@ -0,0 +1,28 @@
+3+4
+
+weight_kg <- 55
+
+mickey <- 27
+x2 <- 3
+
+Weight_kg <- 34
+if
+for
+
+my_mean
+
+MY_MEAN
+
+myMean
+
+my_weight_kg <- 50
+(my_weight <- 50)
+my_weight
+
+2.2*weight_kg
+
+weight_kg <- 57
+2.2*weight_kg
+
+weight_lb <- 2.2*weight_kg
+weight_kg <- 100
