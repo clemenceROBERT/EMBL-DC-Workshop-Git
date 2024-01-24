2c4b4654429c1e03c5875835de3c9f19be9a9693
Clémence Robert <clemence.robert@complifegroup.com>
2024-01-24 10:06:46 +0100

    yay factors

--- scripts/my_third_script.R
+++ scripts/my_third_script.R
@@ -48,5 +48,57 @@ surveys[nrow(surveys), ]
 
 surveys[nrow(surveys)/2, ]
 
-my_list <- list(names= c("Nora", "Lisanna", "Francesco"),
+# my_list <- list(names= c("Nora", "Lisanna", "Francesco"),
                 money = c(1, 6, 7, 3, 5, 8))
+
+# Factors
+str(surveys)
+surveys$sex <- factor(surveys$sex)
+
+levels(surveys$sex)
+nlevels(surveys$sex)
+
+sex <- factor(c("male", "female", "female", "male"))
+sex <- factor(sex, levels = c("male", "female"))
+
+# challenge
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+
+levels(surveys$taxa)
+sum(surveys$taxa== "Rabbit")
+summary(surveys)
+
+nlevels(surveys$genus)
+
+# convert factors
+as.character(sex)
+
+year_fct <- factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct)
+
+as.numeric(as.character(year_fct))
+as.numeric(levels(year_fct))[year_fct]
+
+# Renaming factor
+plot(surveys$sex)
+summary(surveys$sex)
+sex <- surveys$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)
+
+
+levels(sex)[3] <- "undetermined"
+levels(sex)
+sex
+
+
+plot(sex)
+
+
+# Challenge
+levels(sex)[1:2] <- c("female", "male")
+plot(sex)
+sex <- factor(sex, levels=c("undetermined", "female", "male"))
+plot(sex)
